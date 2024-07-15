493a7feb4f5ab4ed6694e36f12ca94d5e09c068f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-15 11:46:56 -0400

    Pass all tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,5 +17,5 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = 'default'
+  # config.order = 'default'
 end
